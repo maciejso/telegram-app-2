48e8eb9e7889d192a4299d5243b6d2840e5fb5fb
Mac Soczka <m.soczka@gmail.com>
2023-11-02 09:09:23 +0000

    link updated

--- Bot/bot.js
+++ Bot/bot.js
@@ -3,7 +3,7 @@ const TOKEN = "6536890026:AAHNrEU4RIrKBWCEmxL8oataVlOwHKDPeGk"
 const TOKEN2= "6495982615:AAHYvtQdqNoIi-5DEljNkwkOS_5_tnpN8k8"
 const bot = new Telegraf(TOKEN2);
 
-const web_link = "https://celebrated-torte-184681.netlify.app/";
+const web_link = "telegram-app-2.vercel.app";
 
 bot.start((ctx) =>
   ctx.reply("Welcome :)))))", {
