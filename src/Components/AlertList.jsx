e747f381ffff493b568395a3d7c74084aff45f2e
Mac Soczka <m.soczka@gmail.com>
2023-11-05 09:50:04 +0000

    edits updated

--- src/Components/AlertList.jsx
+++ src/Components/AlertList.jsx
@@ -4,14 +4,29 @@ import './AlertList.css';
 
 const AlertList = ({ alerts, onEditAlert }) => {
   const [editingAlertId, setEditingAlertId] = useState(null);
+  const [editedAlert, setEditedAlert] = useState({});
 
   const handleEditClick = (alertId) => {
     setEditingAlertId(alertId);
-    onEditAlert(alertId);
+    setEditedAlert(alerts.find((alert) => alert.id === alertId));
   };
 
   const handleCancelEdit = () => {
     setEditingAlertId(null);
+    setEditedAlert({});
+  };
+
+  const handleSaveEdit = () => {
+    onEditAlert(editedAlert);
+    setEditingAlertId(null);
+    setEditedAlert({});
+  };
+
+  const handleInputChange = (field, value) => {
+    setEditedAlert((prevEditedAlert) => ({
+      ...prevEditedAlert,
+      [field]: value,
+    }));
   };
 
   return (
@@ -22,15 +37,28 @@ const AlertList = ({ alerts, onEditAlert }) => {
         {alerts.map((alert) => (
           <li key={alert.id} className={editingAlertId === alert.id ? 'editing' : ''}>
             <div className="alert-details">
-              <p><strong>Cryptocurrency:</strong> {alert.cryptocurrency}</p>
-              <p><strong>Type:</strong> {alert.type}</p>
-              <p><strong>Value:</strong> {alert.value}</p>
-              <p><strong>Currency:</strong> {alert.currency}</p>
-              <p><strong>Expiry Date:</strong> {alert.expiryDate}</p>
+              {editingAlertId === alert.id ? (
+                <>
+                  <p><strong>Date and Time:</strong> <input type="datetime-local" value={editedAlert.datetime} onChange={(e) => handleInputChange('datetime', e.target.value)} /></p>
+                  <p><strong>Cryptocurrency:</strong> <input type="text" value={editedAlert.cryptocurrency} onChange={(e) => handleInputChange('cryptocurrency', e.target.value)} /></p>
+                  <p><strong>Type:</strong> <input type="text" value={editedAlert.type} onChange={(e) => handleInputChange('type', e.target.value)} /></p>
+                  <p><strong>Expiry Date:</strong> <input type="text" value={editedAlert.expiryDate} onChange={(e) => handleInputChange('expiryDate', e.target.value)} /></p>
+                </>
+              ) : (
+                <>
+                  <p><strong>Date and Time:</strong> {alert.datetime}</p>
+                  <p><strong>Cryptocurrency:</strong> {alert.cryptocurrency}</p>
+                  <p><strong>Type:</strong> {alert.type}</p>
+                  <p><strong>Expiry Date:</strong> {alert.expiryDate}</p>
+                </>
+              )}
             </div>
             <div className="alert-actions">
               {editingAlertId === alert.id ? (
-                <button onClick={handleCancelEdit}>Cancel</button>
+                <>
+                  <button onClick={handleCancelEdit}>Cancel</button>
+                  <button onClick={handleSaveEdit}>Save</button>
+                </>
               ) : (
                 <button onClick={() => handleEditClick(alert.id)}>Edit</button>
               )}
