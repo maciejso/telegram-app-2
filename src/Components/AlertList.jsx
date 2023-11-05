99ee5fbad20deff0f1babb6649b2fed228c3424f
Mac Soczka <m.soczka@gmail.com>
2023-11-05 22:39:48 +0000

    formatting

--- src/Components/AlertList.jsx
+++ src/Components/AlertList.jsx
@@ -5,7 +5,7 @@ import './AlertList.css';
 const url = "http://localhost:5000/alerts?state=active"
 
 const AlertList = () => {
-   const [alerts, setAlerts] = useState([]);
+  const [alerts, setAlerts] = useState([]);
   const [editingAlertId, setEditingAlertId] = useState(null);
   const [editedAlert, setEditedAlert] = useState({});
   const [loading, setLoading] = useState(true);
@@ -68,56 +68,56 @@ const AlertList = () => {
       <h2>Alerts List</h2>
       {loading ? (<p>Loading...</p>) : (
 
-      <ul className="alert-list">
-        {alerts.map((alert) => (
-          <li key={alert.id} className={editingAlertId === alert.id ? 'editing' : ''}>
-            <div className="alert-details">
-              {editingAlertId === alert.id ? (
-                <>
-                  <p><strong>Cryptocurrency:</strong> 
-                    <select value={editedAlert.cryptocurrency} onChange={(e) => handleInputChange('cryptocurrency', e.target.value)}>
-                      <option value="Bitcoin">BTC</option>
-                      <option value="Ethereum">ETH</option>
-                      <option value="Ton">TON</option>
-                      {/* Add more cryptocurrencies as needed */}
-                    </select>
-                  </p>
-                  <p><strong>Type:</strong> 
-                    <select value={editedAlert.type} onChange={(e) => handleInputChange('type', e.target.value)}>
-                      <option value="Percent Change">Percent Change</option>
-                      <option value="Value Change">Value Change</option>
-                      {/* Add more types as needed */}
-                    </select>
-                  </p>
-                  <p><strong>Value:</strong> 
-                    <input type="text" value={editedAlert.value} onChange={(e) => handleInputChange('value', e.target.value)} />
-                  </p>
-                  <p><strong>Expiry Date:</strong> 
-                    <input type="datetime-local" value={editedAlert.datetime} onChange={(e) => handleInputChange('datetime', e.target.value)} />
-                  </p>
-                </>
-              ) : (
-                <>
-                  <p><strong>Cryptocurrency:</strong> {alert.cryptocurrency}</p>
-                  <p><strong>Type:</strong> {alert.type}</p>
-                  <p><strong>Value:</strong> {alert.value}</p>
-                  <p><strong>Expiry Date:</strong> {alert.expiryDate}</p>
-                </>
-              )}
-            </div>
-            <div className="alert-actions">
-              {editingAlertId === alert.id ? (
-                <>
-                  <button onClick={handleCancelEdit}>Cancel</button>
-                  <button onClick={handleSaveEdit}>Save</button>
-                </>
-              ) : (
-                <button onClick={() => handleEditClick(alert.id)}>Edit</button>
-              )}
-            </div>
-          </li>
-        ))}
-      </ul>)}
+        <ul className="alert-list">
+          {alerts.map((alert) => (
+            <li key={alert.id} className={editingAlertId === alert.id ? 'editing' : ''}>
+              <div className="alert-details">
+                {editingAlertId === alert.id ? (
+                  <>
+                    <p><strong>Cryptocurrency:</strong>
+                      <select value={editedAlert.cryptocurrency} onChange={(e) => handleInputChange('cryptocurrency', e.target.value)}>
+                        <option value="Bitcoin">BTC</option>
+                        <option value="Ethereum">ETH</option>
+                        <option value="Ton">TON</option>
+                        {/* Add more cryptocurrencies as needed */}
+                      </select>
+                    </p>
+                    <p><strong>Type:</strong>
+                      <select value={editedAlert.type} onChange={(e) => handleInputChange('type', e.target.value)}>
+                        <option value="Percent Change">Percent Change</option>
+                        <option value="Value Change">Value Change</option>
+                        {/* Add more types as needed */}
+                      </select>
+                    </p>
+                    <p><strong>Value:</strong>
+                      <input type="text" value={editedAlert.value} onChange={(e) => handleInputChange('value', e.target.value)} />
+                    </p>
+                    <p><strong>Expiry Date:</strong>
+                      <input type="datetime-local" value={editedAlert.datetime} onChange={(e) => handleInputChange('datetime', e.target.value)} />
+                    </p>
+                  </>
+                ) : (
+                  <>
+                    <p><strong>Cryptocurrency:</strong> {alert.cryptocurrency}</p>
+                    <p><strong>Type:</strong> {alert.type}</p>
+                    <p><strong>Value:</strong> {alert.value}</p>
+                    <p><strong>Expiry Date:</strong> {alert.expiryDate}</p>
+                  </>
+                )}
+              </div>
+              <div className="alert-actions">
+                {editingAlertId === alert.id ? (
+                  <>
+                    <button onClick={handleCancelEdit}>Cancel</button>
+                    <button onClick={handleSaveEdit}>Save</button>
+                  </>
+                ) : (
+                  <button onClick={() => handleEditClick(alert.id)}>Edit</button>
+                )}
+              </div>
+            </li>
+          ))}
+        </ul>)}
     </div>
   );
 };
