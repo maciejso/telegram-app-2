b3a6feab4782710da5da560d6c71354bd538d70c
Mac Soczka <m.soczka@gmail.com>
2023-11-07 22:38:40 +0000

    add logging

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 import Alert from './Components/Alert'
 import AlertList from "./Components/AlertList";
 import Prices from "./Components/Prices";
-import {IAlert, IAlertListProps} from "./models/Alert"
+import { IAlert, IAlertListProps } from "./models/Alert"
 import { ICryptoPrice } from "./models/Price";
 
 
@@ -13,25 +13,27 @@ const App: React.FC = () => {
   const [cryptoPrices, setCryptoPrices] = useState<IAlert[]>([]);
 
   const handleCryptoDataChange = (data: any) => {
-  setCryptoPrices(data);
-};
+    setCryptoPrices(data);
+  };
 
   const tele = (window as any).Telegram.WebApp;
+  function getUserData() {
+      const initDataUnsafe = (window as any).Telegram.WebApp.initDataUnsafe || {};
+      const userId = initDataUnsafe.user && initDataUnsafe.user.id;
+      const username = initDataUnsafe.user && initDataUnsafe.user.username;
+
+      console.log('User ID:', userId);
+      console.log('Username:', username);
+
+  }
+
 
   useEffect(() => {
-  // Set up an event listener for when the Telegram WebApp is ready
-  tele.ready(() => {
-    // Now you can safely call methods on the `tele` object
-    tele.MainButton.show();
-    
-    // If you need to request user data, you should do it here
-    tele.getUser().then((user:any) => {
-      console.log(user);
-    }).catch((error:any) => {
-      console.error(error);
+    tele.onEvent('mainButtonClicked', () => {
+    getUserData();
     });
-  });
-}, []);
+    tele.ready();
+  }, []);
 
   const onAlertUpdate = (newAlert: IAlert) => {
     setAlerts((prevAlerts) => [...prevAlerts, newAlert]);
@@ -46,5 +48,7 @@ const App: React.FC = () => {
     </div>
   );
 };
+
+
 
 export default App;
