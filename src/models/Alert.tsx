d512637bf16b01b74922692026f052903230a734
Mac Soczka <m.soczka@gmail.com>
2023-11-08 10:26:35 +0000

    Revert "interfaces updated"
    This reverts commit 594bf3bb7549e8b848dc0c058979d60e7e51de66.

--- src/models/Alert.tsx
+++ src/models/Alert.tsx
@@ -1,9 +1,7 @@
-import { ICryptoPrice } from "./Price";
 
 export interface IAlert {
-  id: number | undefined
+  id: number;
   cryptocurrency: string;
-  base_value: number,
   trigger_type: string;
   trigger_value: number;
   value: number;
@@ -14,6 +12,6 @@ export interface IAlertListProps {
   alerts: IAlert[];
   setAlerts: React.Dispatch<React.SetStateAction<IAlert[]>>;
   userId: string;
-  cryptoPrices: ICryptoPrice[]; 
 }
+
 
